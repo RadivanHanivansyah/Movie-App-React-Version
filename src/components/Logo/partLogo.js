78eb7cc9b27875a7f49011dd2b3eaa97ff38a1c5
Radivan <radivan100@gmail.com>
2026-05-10 17:15:45 +0700

    menambahkan struktur component form (blm beres)

---
+++ src/components/Logo/partLogo.js
@@ -0,0 +1,21 @@
+import clipLoginRegisterImg from "../../assets/clip login-register.png";
+import chillLoginRegisterImg from "../../assets/CHILL login-register.png";
+import chillBerandaImg from "../../assets/CHILL.png";
+import clipBerandaImg from "../../assets/clip.png";
+
+
+export const part = {
+    containerLoginRegister:
+        "logo flex gap-1 justify-center flex-row items-center mt-2",
+    containerHomePage: "flex gap-1",
+    clipLoginRegister: "w-[50px] h-11 max-sm:w-7 max-sm:h-6",
+    clipLoginRegisterImg: `${ clipLoginRegisterImg }`,
+    chillLoginRegister: "w-[109px] h-11 max-sm:w-[63px] max-sm:h-6",
+    chillLoginRegisterImg: `${ chillLoginRegisterImg }`,
+    clipBeranda: "h-6 max-sm:h-4",
+    clipBerandaImg: `${ clipBerandaImg }`,
+    chillBeranda: "h-6 max-sm:hidden",
+    chillBerandaImg: `${ chillBerandaImg }`,
+
+};
+console.log( part.clipLoginRegisterImg );
