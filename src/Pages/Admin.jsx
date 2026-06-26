6450a3ccd705f3453c1519af8e9cbd01ec90d8af
Radivan <radivan100@gmail.com>
2026-06-26 23:04:43 +0700

    membuat style class button

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -4,7 +4,7 @@ import HeaderAdmin from "../components/Header-Admin/HeaderAdmin";
 export default function Admin() {
   return (
     <div className="bg-slate-200 w-full flex justify-center">
-      <div className="border border-slate-400 rounded-xl py-4 px-4 w-3/4">
+      <div className="border border-slate-400 rounded-xl py-4 px-4 w-3/4 max-sm:w-[90%]">
         <HeaderAdmin />
         <div className="flex flex-wrap gap-x-2 gap-y-4">
           <Card />
